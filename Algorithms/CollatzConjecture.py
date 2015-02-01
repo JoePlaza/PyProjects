728126bbe567c49c1432aa44867800e395ba01bd
JoePlaza <plazauk@hotmail.com>
2015-02-01 17:47:43 +0000

    Create CollatzConjecture.py

---
+++ Algorithms/CollatzConjecture.py
@@ -0,0 +1,17 @@
+# Collatz conjecture
+
+def collatz(num):
+    goes=0
+    while num > 1:
+        if num%2==0:
+            goes += 1
+            num = num/2
+
+        elif num%2 != 0:
+            goes +=1
+            num = (num*3)+1
+
+    print("It took %s goes" %goes)
+
+
+collatz(num)
